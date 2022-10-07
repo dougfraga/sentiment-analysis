12f34e4b171e133e106250481ce090c362cf95e3
Douglas Rodrigues <douglas.fraga@gmail.com>
2022-10-07 14:33:46 -0300

    add classificador

--- base.py
+++ base.py
@@ -1,5 +1,7 @@
 from gerenciador import Gerenciador
 from vetorizador import CountVectorizer, TFIDFVectorizer
+from classificador import NaiveBayes, SVM
+from util import analisar_sentimento
 
 gerenciador = Gerenciador()
 revisoes = gerenciador.revisoes
@@ -10,8 +12,31 @@ vec2 = CountVectorizer(revisoes, (1,4))
 vec3 = TFIDFVectorizer(revisoes)
 vec4 = TFIDFVectorizer(revisoes, (1,4))
 
-print(len(vec1.vetorizador.vocabulary_))
-print(len(vec2.vetorizador.vocabulary_))
-print(len(vec3.vetorizador.vocabulary_))
-print(len(vec4.vetorizador.vocabulary_))
+class1 = NaiveBayes("naive_bayes_1_1.pickle", vec1, recomendacoes)
+class2 = NaiveBayes("naive_bayes_1_4.pickle", vec2, recomendacoes)
+class3 = SVM("svm_1_1.pickle", vec3, recomendacoes)
+class4 = SVM("svm_1_4.pickle", vec4, recomendacoes)
 
+testar = True
+
+while testar is True:
+    texto = input("\n>>> ")
+
+    if texto == 0:
+        testar = False
+    else:
+        analisar_sentimento(class1, texto)
+        analisar_sentimento(class2, texto)
+        analisar_sentimento(class3, texto)
+        analisar_sentimento(class4, texto)
+
+
+#class1 = NaiveBayes("naive_bayes_1_1.pickle")
+#class2 = NaiveBayes("naive_bayes_1_4.pickle")
+#class3 = SVM("svm_1_1.pickle")
+#class4 = SVM("svm_1_4.pickle")
+
+#print(class1.marcador())
+#print(class2.marcador())
+#print(class3.marcador())
+#print(class4.marcador())
---
+++ util.py
@@ -0,0 +1,6 @@
+def analisar_sentimento(classificador, texto):
+    mensagem = "<!> Esse texto é considerado"
+    predicao = classificador.preditor_texto(texto)
+    mensagem += (" positivo pelo classificador %s."%classificador.nome) if int(predicao[0]) > 0 else (" negativo pelo classificador %s"%classificador.nome)
+    print(mensagem)
+
