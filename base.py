e69e2330bbb4a4a72a50e8336d2a136d4ad47e49
Douglas Rodrigues <douglas.fraga@gmail.com>
2022-10-07 00:22:55 -0300

    add vetorizador

--- base.py
+++ base.py
@@ -1,9 +1,17 @@
 from gerenciador import Gerenciador
+from vetorizador import CountVectorizer, TFIDFVectorizer
 
 gerenciador = Gerenciador()
 revisoes = gerenciador.revisoes
 recomendacoes = gerenciador.recomendacoes
 
-print(revisoes[4999])
-print(recomendacoes)
+vec1 = CountVectorizer(revisoes)
+vec2 = CountVectorizer(revisoes, (1,4))
+vec3 = TFIDFVectorizer(revisoes)
+vec4 = TFIDFVectorizer(revisoes, (1,4))
+
+print(len(vec1.vetorizador.vocabulary_))
+print(len(vec2.vetorizador.vocabulary_))
+print(len(vec3.vetorizador.vocabulary_))
+print(len(vec4.vetorizador.vocabulary_))
 
