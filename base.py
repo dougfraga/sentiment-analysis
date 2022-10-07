665ff70c2cd963cc62a4d7e2a59235d4141dde41
Douglas Rodrigues <douglas.rodrigues@oceanpact.com>
2022-10-06 23:36:57 -0300

    First updates

---
+++ base.py
@@ -0,0 +1,9 @@
+from gerenciador import Gerenciador
+
+gerenciador = Gerenciador()
+revisoes = gerenciador.revisoes
+recomendacoes = gerenciador.recomendacoes
+
+print(revisoes[4999])
+print(recomendacoes)
+
